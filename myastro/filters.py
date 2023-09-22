966fb9b9d4169c8fb47ff1a2cb3f2e9f459e1f6d
Dries Van De Putte <dvandeputte@stsci.edu>
2023-09-21 21:14:36 -0400

    Additions to synthetic photometry stuff

--- myastro/filters.py
+++ myastro/filters.py
@@ -4,11 +4,12 @@ Also has stuff to download filters and load them as objects of my simple
 classes.
 
 """
-
+from itertools import cycle
 import stsynphot
 import numpy as np
 from scipy.interpolate import interp1d
 from astropy import units as u
+from matplotlib import pyplot as plt
 
 
 class FilterCurve:
@@ -61,12 +62,18 @@ def get_HST_filter_curve(stsynphotstring, wmin, wmax):
     return DataFilterCurve(s.wavelength.to(u.micron).value, s.flux.value)
 
 
+def get_stsynphot_filter_curve(string, wmin, wmax):
+    bp = stsynphot.band(string).to_spectrum1d()
+    s = bp[wmin * u.micron : wmax * u.micron]
+    return DataFilterCurve(s.wavelength.to(u.micron).value, s.flux.value)
+
+
 def calc_sed(wavs, flux, filters):
     """Calculate SED for given flux spectrum and set of filters.
 
     wavs and flux are just arrays (no units)
 
-    filters is a list of FilterCurve objects
+    filters: list of FilterCurve objects
     """
     M = len(filters)
     wavcenters = np.zeros(M)
@@ -76,3 +83,65 @@ def calc_sed(wavs, flux, filters):
         sed[i] = f.apply(wavs, flux)
 
     return wavcenters, sed
+
+
+def plot_spectrum_filters_and_sed(
+    ax_sed, ax_filter, wavs, flux, filters, extra_fluxes=[], text_pos=None
+):
+    """ax_filter: recommendation: a twin axis of ax_sed
+
+    wavs: wavelengths in the units supported by the given filter objects
+
+    flux: flux array (units don't matter)
+
+    filters: dict {str: FilterCurve}
+        labels and FilterCurve objects
+
+    extra fluxes: list of array
+        extra curves, will use a line style cycle
+
+    text_pos: list of str
+        where to put the filter labels on the filter axes (vertical
+        position). Defaults to 0.5.
+
+    """
+    wavcenters, sed = calc_sed(wavs, flux, filters.values())
+
+    # filters
+    prop_cycle = plt.rcParams["axes.prop_cycle"]
+    colors = prop_cycle.by_key()["color"]
+    for i, name in enumerate(filters):
+        t = filters[name].evaluate(wavs)
+        mask = t != 0
+        ax_filter.plot(wavs[mask], t[mask], color=colors[i], alpha=0.5)
+
+        if text_pos is None:
+            text_y = np.average(flux)
+        else:
+            text_y = text_pos[i]
+
+        ax_sed.text(
+            wavcenters[i],
+            text_y,
+            name,
+            # name.split("_")[-1],
+            color=colors[i],
+            ha="center",
+            rotation=90,
+        )
+
+    # spectrum
+    ax_sed.plot(wavs, flux, color="k")
+
+    # sed
+    ax_sed.scatter(wavcenters, sed, c=colors[: len(sed)])
+
+    # same with an extra spectrum (plotted at low alpha or dotted)
+    styles = cycle(("dotted", "dashed"))
+    for ef in extra_fluxes:
+        _, ref_sed = calc_sed(wavs, ef, filters.values())
+        ax_sed.plot(wavs, ef, color="k", alpha=0.5, linestyle=next(styles))
+        ax_sed.scatter(wavcenters, ref_sed, c=colors[: len(sed)], alpha=0.5)
+
+    ax_sed.set_xlabel("wavelength (micron)")
+    ax_filter.set_ylabel("Filter transmission")
