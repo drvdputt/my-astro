1f3bb98e4451a3cbc7df0bf2a7406cb4d6af8789
Dries Van De Putte <dvandeputte@stsci.edu>
2023-10-06 14:41:40 -0400

    Improvements to download csv

--- scripts/download_csv.py
+++ scripts/download_csv.py
@@ -9,8 +9,9 @@ print("opening ", argv[1])
 # obsid_table = Table.read('/Users/dvandeputte/Downloads/Proposal_IDs_1288(1).csv')
 obsid_table = Table.read(argv[1], comment="#")
 
-# custom filter
-keep = np.array(["c1002" in row["obs_id"] for row in obsid_table])
+# custom filter example
+# keep = np.array(["c1007" in row["obs_id"] for row in obsid_table])
+keep = np.full(len(obsid_table), True)
 
 obsids = obsid_table[keep]["obs_id"]
 allobs = Observations.query_criteria(obs_id=obsids)
@@ -18,12 +19,16 @@ allobs = Observations.query_criteria(obs_id=obsids)
 for obs in allobs:
     print(obs)
     products = Observations.get_product_list(obs)
+    print("entire product list")
+    print(products)
+
     fits_products = Observations.filter_products(
         products, productType=["SCIENCE"], extension="fits", calib_level=[1]
     )
+    print("filtered product list")
     print(fits_products)
 
-    if argv[2] == "y":
+    if len(argv) > 2 and argv[2] == "y":
         answer = "y"
     else:
         answer = input("download these? y/n")
