07611129ad62a723d0188fd071fc9bd6e2da4003
Dries Van De Putte <dvandeputte@stsci.edu>
2023-10-09 11:44:20 -0400

    STIS sx2 loading function

--- myastro/load.py
+++ myastro/load.py
@@ -5,6 +5,10 @@ that globs and sorts the 12 MIRI cubes
 
 from specutils import Spectrum1D
 from pathlib import Path
+from astropy.io import fits
+from astropy.wcs import WCS
+from astropy import units as u
+from astropy.nddata import StdDevUncertainty
 
 def get_cubes(d, suffix="s3d.fits"):
     """Get all cubes in a directory, sorted by wavelength
@@ -18,3 +22,11 @@ def get_cubes(d, suffix="s3d.fits"):
     if len(cubes) == 0:
         raise FileExistsError(f"No cubes found in {d}")
     return cubes
+
+def stis_sx2(fn):
+    with fits.open(fn) as hdul:
+        sci = hdul['SCI']
+        bunit = u.Unit(sci.header['BUNIT'])
+        s = Spectrum1D(sci.data * bunit, wcs=WCS(sci), uncertainty=StdDevUncertainty(hdul['ERR'].data * bunit))
+        s.meta['header'] = sci.header
+    return s
