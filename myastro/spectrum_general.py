60566b9474ed688a3dc23f610d394b7da02602c2
Dries Van De Putte <dvandepu@uwo.ca>
2024-09-06 19:04:03 -0400

    Some spectrum_general functions take Spectrum1D or wavelenght+flux
    This was achieved by setting up a decorator

--- myastro/spectrum_general.py
+++ myastro/spectrum_general.py
@@ -2,19 +2,28 @@
 
 from specutils import Spectrum1D
 import numpy as np
+from astropy import units as u
+from astropy.table import Table
+
 
 def mask_wavelength_range(s1d, wmin, wmax):
     return np.logical_and(wmin < s1d.spectral_axis, s1d.spectral_axis < wmax)
+
 
 def slice_spectral_axis(s1d, mask):
     """Arbitrary slice of spectral axis.
 
     Need to rebuild object to do this
     """
-    s1d_new = Spectrum1D(s1d.flux[mask], s1d.spectral_axis[mask], uncertainty=None if s1d.uncertainty is None else s1d.uncertainty[mask])
+    s1d_new = Spectrum1D(
+        s1d.flux[mask],
+        s1d.spectral_axis[mask],
+        uncertainty=None if s1d.uncertainty is None else s1d.uncertainty[mask],
+    )
     s1d_new.meta = s1d.meta
     return s1d_new
-    
+
+
 def coadd(s1ds, new_spectral_axis=None):
     """Co-add any list of Spectrum1D objects.
 
@@ -31,3 +40,60 @@ def coadd(s1ds, new_spectral_axis=None):
     """
     pass
 
+
+def take_s1d_or_quantities(func):
+    def decorated_func(
+        *args, s1d=None, wavelength=None, flux=None, uncertainty=None, **kwargs
+    ):
+        if s1d is not None:
+            w = s1d.spectral_axis
+            f = s1d.flux
+            u = s1d.uncertainty
+        else:
+            w = wavelength
+            f = flux
+            u = uncertainty
+
+        kwargs["wavelength"] = w
+        kwargs["flux"] = f
+        kwargs["uncertainty"] = u
+
+        return func(*args, **kwargs)
+
+    return decorated_func
+
+
+@take_s1d_or_quantities
+def write_ascii_etc(fn, wavelength, flux, uncertainty):
+    """Write a spectrum1d in ascii compatible with HST ETCs"""
+    flux_etc = flux.to(u.erg * u.cm**-2 * u.s**-1 * u.angstrom**-1).value
+    wavelength_etc = wavelength.to(u.angstrom).value
+    good = (flux_etc > 0) & (wavelength_etc > 0) & np.isfinite(flux_etc)
+    np.savetxt(fn, np.column_stack((wavelength_etc[good], flux_etc[good])))
+
+
+@take_s1d_or_quantities
+def write_ecsv(fn, wavelength, flux, uncertainty):
+    """Write spectrum (in astropy quantities) to ecsv file
+
+    s1d: Spectrum1D
+        If not given, then the individual wavelength, flux, uncertainty
+        arguments should be used.
+
+    wavelength: Quantity
+
+    flux: Quantity
+
+    uncertainty: array like or e.g. StdDevUncertainty
+        (will be converted to array and multiplied with flux unit)
+
+    fn: str
+        file name, best is to use ECSV
+
+    """
+    t = Table()
+    t.add_column(wavelength, name="wavelength")
+    t.add_column(flux, name="flux")
+    if uncertainty is not None:
+        t.add_column(uncertainty.array * flux.unit)
+    t.write(fn)
