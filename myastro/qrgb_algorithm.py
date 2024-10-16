05b32541d3227c35798d68e040d96d16f75ad9fe
Dries Van De Putte <dvandepu@uwo.ca>
2024-10-16 17:54:08 -0400

    Add QRGB algorithm

---
+++ myastro/qrgb_algorithm.py
@@ -0,0 +1,117 @@
+"""This a a place to put the whole thing (a bit too extensive for
+myastro, at least until this repo+mystro get merged."""
+
+import matplotlib
+from astropy.nddata import CCDData
+import reproject
+import numpy as np
+from myastro import image, rgb
+from astropy.visualization import make_lupton_rgb
+
+
+class QRGB:
+    """
+    Attributes
+    ----------
+
+    rgb_method: str
+        Options are "naive" and "lupton"
+    """
+    def __init__(self, images_array):
+        """
+        Parameters
+        ----------
+
+        images_array: 3D array
+            All images to be used for the RGB image, packed in one
+            single 3D cube. Indices are: image, iy, ix
+
+        """
+        self.images = images_array
+        nc = self.images.shape[0]
+
+        # normalization options
+        self.clip_pmin = 0
+        self.clip_pmax = 100
+        self.offsets = [0] * nc
+
+        # colors to use for data -> rgb conversion
+        cmap = matplotlib.cm.get_cmap("hsv")
+        self.colors = [cmap(i / nc) for i in range(nc)]
+
+        # rgb combination options
+        self.rgb_method = 'naive'
+        self.stretch == 1
+        self.Q = 1
+
+    @classmethod
+    def read(cls, fits_files, wcs_index: int = 0):
+        """Quickly set up QRGB by reading multiple fits files and
+        reprojecting them all"""
+
+        ccddata = [CCDData.read(fn) for fn in fits_files]
+
+        images = []
+        for i in range(len(ccddata)):
+            # skip reprojection for the one we are basing the WCS on
+            if i == wcs_index:
+                images.append(ccddata[i].data)
+                continue
+
+            image, footprint = reproject.reproject_interp(
+                ccddata[i],
+                ccddata[wcs_index].wcs,
+                shape_out=ccddata[wcs_index].shape,
+                order="nearest-neighbor",
+            )
+
+        images_array = np.stack(images, axis=0)
+        return cls(images_array)
+
+    def get_normalized_images(self):
+        """Clean and renormalize the input data
+
+        Used internally mostly, but you can call it to check what your
+        clip, scale, and offset options are doing.
+
+        Returns
+        -------
+
+        List of 2d arrays, same length as self.images
+        """
+        kwargs = dict(
+            clip_pmin=self.clip_pmin,
+            clip_pmax=self.clip_pmax,
+            scale_pmin=self.scale_pmin,
+            scale_pmax=self.scale_pmax,
+        )
+        return [
+            image.normalize(self.images[i], offset_p=self.offsets[i] ** kwargs)
+            for i in range(self.nc)
+        ]
+
+    def get_rgb(self):
+        """Apply the full algorithm to make imshowable rgb array
+
+        1. normalize images
+        2. combine arbitrary colors into RGB
+        3. convert RGB to integer normalized RGB, with a choice of method
+
+        Returns
+        -------
+
+        uint8 array, shape (ny, nx, rgb), compatible with imshow
+
+        """
+        normalized_images = self.get_normalized_images()
+        r, g, b = rgb.combine_arbitrary_colors(normalized_images, self.colors)
+
+        # apply curve/stretch and combine RGB
+        if self.rgb_method == "lupton":
+            rgb_uint8 = make_lupton_rgb(r, g, b, stretch=self.stretch, Q=self.Q)
+        elif self.color_method == "naive":
+            rgb_uint8 = rgb.make_naive_rgb(r, g, b, stretch=self.stretch, Q=self.Q)
+        else:
+            raise "unsupported color method"
+
+        return rgb_uint8
--- myastro/rgb.py
+++ myastro/rgb.py
@@ -1,6 +1,7 @@
 """Some tools to quickly make rgb images the way I like it."""
 
 import numpy as np
+import matplotlib
 
 
 def asinh_curve(image, scale, offset):
@@ -70,3 +71,37 @@ def make_naive_rgb(image_r, image_g, image_b, stretch=5, Q=8):
     """
     rgb = [scale_asinh_255(i, stretch, Q) for i in (image_r, image_g, image_b)]
     return np.stack(rgb, axis=-1)
+
+def combine_arbitrary_colors(images, colors):
+    """Make 3-channel rgb from many channels with different colors.
+
+    Parameters
+    ----------
+
+    image_list: list of 2d arrays (same shape)
+        Preferably, these input monochromatic images are already
+        processed by something like image.normalize. For the best
+        results, the images should be of the same order of magnitude,
+        and should not have extreme outliers.
+
+    color_list: list of colors
+        Can be anything compatible with matplotlib.colors.to_rgba()
+
+    Returns
+    -------
+
+    list of 3 float arrays
+
+    Note: these return values still need to be processed by make_naive_rgb or make_lupton_rgb
+
+    """
+    nx, ny = images[0].shape
+    shape = (3, nx, ny)
+    rgb_totals = np.zeros(shape)
+    for array, color in zip(images, colors):
+        rgba = matplotlib.colors.to_rgba(color)
+        for c in range(3):
+            rgb_totals[c] += array * rgba[c]
+
+    # return as list for consistency with the rest of the code
+    return [array for array in rgb_totals]
