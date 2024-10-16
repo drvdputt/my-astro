633102c12ba13ac3f46d17945442a59d40d2c69a
Dries Van De Putte <dvandepu@uwo.ca>
2024-10-16 13:35:15 -0400

    Image and rgb tools for color image making

--- myastro/image.py
+++ myastro/image.py
@@ -1,7 +1,14 @@
-"""Some very common things to do with images, including plotting and comparing."""
+"""Some very common things to do with images, including plotting and comparing.
+
+The functions here deal with monochromatic image data (simple 2d
+arrays). Anything related to combining images into colors, should go
+into rgb.py.
+
+"""
 
 from myastro import plot
 from matplotlib import pyplot as plt
+import numpy as np
 
 def plot_residual(a1, a2):
     """Show two arrays and their difference.
@@ -23,3 +30,36 @@ def plot_residual(a1, a2):
     fig.tight_layout()
     fig.subplots_adjust(hspace=0)
     return fig, ax
+
+def normalize(
+    channel_array, clip_pmin=0, clip_pmax=100, scale_pmin=16, scale_pmax=84, offset_p=0
+):
+    """Apply cleaning and move images to the order of magnitude 1
+
+    Steps:
+
+    1. Clip using clip_pmin and clip_pmax as cutoff percentiles.
+    Everything below pmin is set to the pmin percentile flux value.
+    Everything above pmax is set to the pmax percentile flux value.
+
+    2. Subtracts offset using the offset percentile. Everying below
+    offset_p will be set to 0 (TODO: or negative? What is best?)
+
+    3. Rescale the data so that all points between the scale_pmin and
+    scale_pmax percentiles run from 0 to 1. Everything above scale_pmax
+    will still be greater than 1. The idea is that the most important
+    part of the dynamic range happens around 0.5.
+
+    Returns
+    -------
+    modified image : array of floats
+
+    """
+    cmin = np.nanpercentile(channel_array, clip_pmin)
+    cmax = np.nanpercentile(channel_array, clip_pmax)
+    image = np.maximum(channel_array, cmin)
+    image = np.minimum(image, cmax)
+
+    width = np.nanpercentile(image, scale_pmax) - np.nanpercentile(image, scale_pmin)
+    new_image = (image - np.nanpercentile(image, offset_p)) / width
+    return new_image
--- myastro/rgb.py
+++ myastro/rgb.py
@@ -2,6 +2,7 @@
 
 import numpy as np
 
+
 def asinh_curve(image, scale, offset):
     """curve(x) = asinh(scale * (x - offset))
 
@@ -25,43 +26,47 @@ def asinh_curve(image, scale, offset):
     """
     return np.arcsinh(scale * (image - offset))
 
-def dumb_scaling(image):
-    """Scales a monochromatic image to the 0 - 255 range
+
+def scale_asinh_255(image, stretch=5, Q=8):
+    """Apply asinh stretch, scales monochromatic image to 0 - 255 range.
+
+    Assumes that the data have already been cleaned
+    For the most predictable results, remove negative values from your
+    data at the start, by clipping or applying a suitable bias yourself.
+
+    If there are still negative values, they will be clipped (set to 0)
+    here.
+
+    Parameters
+    ----------
+
+    image: 2d array
 
     Returns
     -------
-    array of float
+    uint8 2d array
 
     """
     new_image = image.copy()
-    new_image = np.sqrt(image)
-    imin = np.nanpercentile(new_image, 0)
-    span = np.nanpercentile(new_image, 100) - imin
-    new_image = (new_image - imin) / span * 255
-    return new_image
+    new_image = Q * np.arcsinh(Q * image / stretch)
+    # configurable percentiles later?
+
+    # ensure there are no negative values
+    new_image[new_image < 0] = 0
+
+    new_image = new_image / np.nanmax(new_image) * 255
+    return new_image.astype("uint8")
 
 
-def normalize(
-    image, clip_pmin=0, clip_pmax=100, scale_pmin=16, scale_pmax=84, offset_p=50
-):
-    """Bring images into a similar order of magnitude.
+def make_naive_rgb(image_r, image_g, image_b, stretch=5, Q=8):
+    """Naive version with similar signature as lupton rgb function.
 
-    Rescales using a width, determined given the scale percentile range
+    Even though this is less informative, it looks better aesthetically
+    sometimes.
 
-    Clips the image using the given percentiles
-
-    Centers the image values by subtracting the offset percentile offset_p
-
-    Returns
-    -------
-    modified image : array of floats
+    We try to recreate some of the same arguments for the curve, but the
+    stretch here is deliberately NOT color preserving.
 
     """
-    cmin = np.nanpercentile(image, clip_pmin)
-    cmax = np.nanpercentile(image, clip_pmax)
-    image = np.maximum(image, cmin)
-    image = np.minimum(image, cmax)
-
-    width = np.nanpercentile(image, scale_pmax) - np.nanpercentile(image, scale_pmin)
-    new_image = (image - np.nanpercentile(image, offset_p)) / width
-    return new_image
+    rgb = [scale_asinh_255(i, stretch, Q) for i in (image_r, image_g, image_b)]
+    return np.stack(rgb, axis=-1)
